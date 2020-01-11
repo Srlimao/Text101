2de92ca9185fb9bc9b49c78cf38f0c1db4885c73
Srlimao <w.back93@gmail.com>
2020-01-11 00:21:13 -0300

    Finish

---
+++ Assets/Script/Game.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] State startingState;
+
+    [SerializeField] Text titleText;
+    [SerializeField] Text storyText;
+    [SerializeField] Text optionsText;
+
+
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        LoadStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+        LoadStory();
+
+    }
+
+    private void LoadStory()
+    {
+        titleText.text = state.GetStateTitle();
+        storyText.text = state.GetStateStory();
+        optionsText.text = state.GetStateOptions();
+    }
+
+    private void ManageState()
+    {
+        var nextState = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextState[0];
+        }else 
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextState[1];
+        }
+        else
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextState[2];
+        }
+    }
+}
---
+++ Assets/Script/State.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [SerializeField] string storyTitle;
+    [TextArea(14, 10)] [SerializeField] string storyText;
+    [TextArea(6, 4)] [SerializeField] string storyOptions;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public string GetStateTitle()
+    {
+        return storyTitle;
+    }
+
+    public string GetStateOptions()
+    {
+        return storyOptions;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
